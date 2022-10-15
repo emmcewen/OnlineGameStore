af01963ea34111306034d62c253ead1d63227dca
Adrian Firmalan <afirmalan@yahoo.com>
2022-10-15 14:00:49 -0400

    starter code for resolver and cart

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,10 @@
+// const variables: Auth, User Models, Apollo
+
+// resolver const 
+
+// -- Game find with genre, title , args, context
+
+
+// throw Auth error if user is not logged in
+
+// Send game element to checkout and populate checkot
