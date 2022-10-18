653f78548e90a3b20f9d9f93d4f4b928fc272a91
Mariah <mariahjohnston217@outlook.com>
2022-10-17 23:58:38 -0400

    changed card styling, added a few more cards, added all 20 card images to image file

--- client/src/pages/Store.js
+++ client/src/pages/Store.js
@@ -14,8 +14,8 @@ import '../styles/style.css'
 export default function Store() {
   return (
     <>
-      <Card className="w-96" id='cards1'>
-        <CardHeader color="blue" className="relative h-56">
+      <Card className="w-96 mt-60 bg-gold text-black" id='card1'>
+        <CardHeader className="relative h-56">
           <img
             src="/img/MinecraftCard.jpg"
             alt="img-blur-shadow"
@@ -36,94 +36,101 @@ export default function Store() {
           <Button><FaRegThumbsDown></FaRegThumbsDown></Button>
         </CardFooter>
       </Card>
-      <Card className="w-96" id='cards1'>
-        <CardHeader color="blue" className="relative h-56">
+
+      <Card className="w-96 mt-20 bg-gold text-black" id='card2'>
+        <CardHeader className="relative h-56">
           <img
-            src="/img/MinecraftCard.jpg"
+            src="/img/ApexCard.jpg"
             alt="img-blur-shadow"
             className="h-full w-full"
           />
         </CardHeader>
         <CardBody className="text-center">
           <Typography variant="h5" className="mb-2">
-            Minecraft Dungeons
+            Apex Legends
           </Typography>
           <Typography>
-            Fight your way through an exciting action-adventure game, inspired by classic dungeon crawlers and set in the Minecraft universe!
+       Master an ever-growing roster of legendary characters with powerful abilities, and experience strategic squad play and innovative gameplay in the next evolution of Hero Shooter and Battle Royale.
+
           </Typography>
         </CardBody>
         <CardFooter divider className="flex items-center justify-between py-3">
-          <Typography variant="small">$19.99</Typography>
+          <Typography variant="small">$Free</Typography>
           <Button><FaRegThumbsUp></FaRegThumbsUp></Button>
           <Button><FaRegThumbsDown></FaRegThumbsDown></Button>
         </CardFooter>
       </Card>
-      <Card className="w-96" id='cards1'>
-        <CardHeader color="blue" className="relative h-56">
+
+      <Card className="w-96 mt-20 bg-gold text-black" id='card3'>
+        <CardHeader className="relative h-56">
           <img
-            src="/img/MinecraftCard.jpg"
+            src="/img/LostArkCard.jpg"
             alt="img-blur-shadow"
             className="h-full w-full"
           />
         </CardHeader>
         <CardBody className="text-center">
           <Typography variant="h5" className="mb-2">
-            Minecraft Dungeons
+            Lost Ark
           </Typography>
           <Typography>
-            Fight your way through an exciting action-adventure game, inspired by classic dungeon crawlers and set in the Minecraft universe!
+          Embark on an odyssey for the Lost Ark in a vast, vibrant world: explore new lands, seek out lost treasures, and test yourself in thrilling action combat in this action-packed free-to-play RPG.
           </Typography>
         </CardBody>
         <CardFooter divider className="flex items-center justify-between py-3">
-          <Typography variant="small">$19.99</Typography>
+          <Typography variant="small">$Free</Typography>
           <Button><FaRegThumbsUp></FaRegThumbsUp></Button>
           <Button><FaRegThumbsDown></FaRegThumbsDown></Button>
         </CardFooter>
       </Card>
-      <Card className="w-96" id='cards1'>
-        <CardHeader color="blue" className="relative h-56">
+
+      <Card className="w-96 mt-20 bg-gold text-black" id='card4'>
+        <CardHeader className="relative h-56">
           <img
-            src="/img/MinecraftCard.jpg"
+            src="/img/BlackDesertCard.jpg"
             alt="img-blur-shadow"
             className="h-full w-full"
           />
         </CardHeader>
         <CardBody className="text-center">
           <Typography variant="h5" className="mb-2">
-            Minecraft Dungeons
+            Black Desert
           </Typography>
           <Typography>
-            Fight your way through an exciting action-adventure game, inspired by classic dungeon crawlers and set in the Minecraft universe!
+          Black Desert Online is an open-world, action MMORPG. Experience intense, action-packed combat, battle massive world bosses, fight alongside friends to siege and conquer castles, and train in professions such as fishing, trading, crafting, cooking, and more!
           </Typography>
         </CardBody>
         <CardFooter divider className="flex items-center justify-between py-3">
-          <Typography variant="small">$19.99</Typography>
+          <Typography variant="small">$9.99</Typography>
           <Button><FaRegThumbsUp></FaRegThumbsUp></Button>
           <Button><FaRegThumbsDown></FaRegThumbsDown></Button>
         </CardFooter>
       </Card>
-      <Card className="w-96" id='cards1'>
-        <CardHeader color="blue" className="relative h-56">
+
+      <Card className="w-96 mt-20 bg-gold text-black" id='card5'>
+        <CardHeader className="relative h-56">
           <img
-            src="/img/MinecraftCard.jpg"
+            src="/img/BlackDesertCard.jpg"
             alt="img-blur-shadow"
             className="h-full w-full"
           />
         </CardHeader>
         <CardBody className="text-center">
           <Typography variant="h5" className="mb-2">
-            Minecraft Dungeons
+            Black Desert
           </Typography>
           <Typography>
-            Fight your way through an exciting action-adventure game, inspired by classic dungeon crawlers and set in the Minecraft universe!
+          Black Desert Online is an open-world, action MMORPG. Experience intense, action-packed combat, battle massive world bosses, fight alongside friends to siege and conquer castles, and train in professions such as fishing, trading, crafting, cooking, and more!
           </Typography>
         </CardBody>
         <CardFooter divider className="flex items-center justify-between py-3">
-          <Typography variant="small">$19.99</Typography>
+          <Typography variant="small">$9.99</Typography>
           <Button><FaRegThumbsUp></FaRegThumbsUp></Button>
           <Button><FaRegThumbsDown></FaRegThumbsDown></Button>
         </CardFooter>
       </Card>
+
+
     </>
   )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,14 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gold': '#e4a32f',
+        'black': '#000000',
+        'pink': '#FBBAF7',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+  ], 
 });
