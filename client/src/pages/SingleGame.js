6d1bdd16ac8d9d217ba79d9e8de7d6338392d110
Mariah <mariahjohnston217@outlook.com>
2022-10-20 22:32:55 -0400

    added static image and card to singlegamepage to try to get some styling, added some custom css

--- client/src/pages/SingleGame.js
+++ client/src/pages/SingleGame.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
   Card,
-  CardHeader,
   CardBody,
   CardFooter,
   Typography,
@@ -10,36 +9,38 @@ import {
 export default function SingleGame() {
   return (
     <>
-      <img
+      <img class="mx-auto"
         src='/img/minecraft.jpg'
-        className='h-1/3 w-1/3'
+        className='h-1/3 w-1/3 mt-40'
       />
-      <Card className="w-96">
-        <CardBody className="text-center">
-          <Typography variant="h5" className="mb-2">
-            About
-          </Typography>
-          <Typography>
-            Minecraft Dungeons: <br />
-            Fight your way through an exciting action-adventure game, inspired by classic dungeon crawlers and set in the Minecraft universe! Brave the dungeons alone, or team up with friends! Up to four players can battle together through action-packed, treasure-stuffed, wildly varied levels – all in an epic quest to save the villagers and take down the evil Arch-Illager!
-          </Typography>
-        </CardBody>
-        <CardFooter divider className="flex items-center justify-between py-3">
-          <Typography variant="small">$19.99 
-          <br />
-          <br />
-          ESRB: Everyone 10+
-          </Typography>
-          <Typography variant="small" color="gray" className="flex gap-1">
-            <i className="fas fa-map-marker-alt fa-sm mt-[3px]" />
-            <div className='addtocart'>
-              <button className='addtocartbtn mr-20'>Add to cart<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-              </svg></button>
-            </div>
-          </Typography>
-        </CardFooter>
-      </Card>
+      <div class='card'>
+        <Card className="h-auto bg-blue bg-opacity-60 text-grey">
+          <CardBody className="text-center">
+            <Typography variant="h5" className="mb-2">
+              About
+            </Typography>
+            <Typography>
+              Minecraft Dungeons: <br />
+              Fight your way through an exciting action-adventure game, inspired by classic dungeon crawlers and set in the Minecraft universe! Brave the dungeons alone, or team up with friends! Up to four players can battle together through action-packed, treasure-stuffed, wildly varied levels – all in an epic quest to save the villagers and take down the evil Arch-Illager!
+            </Typography>
+          </CardBody>
+          <CardFooter divider className="flex items-center justify-between py-3">
+            <Typography variant="small">$19.99
+              <br />
+              <br />
+              ESRB: Everyone 10+
+            </Typography>
+            <Typography variant="small" color="gray" className="flex gap-1">
+              <i className="fas fa-map-marker-alt fa-sm mt-[3px]" />
+              <div className='addtocart'>
+                <button className='addtocartbtn mr-20 text-grey'>Add to cart<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg></button>
+              </div>
+            </Typography>
+          </CardFooter>
+        </Card>
+      </div>
     </>
   )
 }
