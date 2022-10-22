ffbbf4d5e5af460d302c83b7beba4cc53fdca999
Mariah <mariahjohnston217@outlook.com>
2022-10-22 14:03:24 -0400

    imported login and signup to gamecontainer, removed imports from app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import GameContainer from './components/GameContainer';
 import "./output.css"
-import Login from './pages/Login';
-import Signup from './pages/Signup';
 export default function App() {
     return (
         <>
