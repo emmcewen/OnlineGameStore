4a9bb56894efce6fe46990a25e5a38c21ed587ea
Mariah <mariahjohnston217@outlook.com>
2022-10-22 23:31:02 -0400

    added apolloprovider wrap in app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import PageContainer from './components/PageContainer';
 import "./output.css"
 export default function App() {
     return (
         <>
-            <PageContainer />
+            <ApolloProvider>
+                <PageContainer />
+            </ApolloProvider>
         </>
     )
 }
--- client/src/pages/SingleGame.js
+++ client/src/pages/SingleGame.js
@@ -1,45 +1,45 @@
-import React from 'react';
-import {
-  Card,
-  CardBody,
-  CardFooter,
-  Typography,
-} from '@material-tailwind/react'
-import { QUERY_SINGLE_GAME } from '../utils/queries';
-import AddCartBtn from '../components/AddCartBtn'
+// import React from 'react';
+// import {
+//   Card,
+//   CardBody,
+//   CardFooter,
+//   Typography,
+// } from '@material-tailwind/react'
+// import { QUERY_SINGLE_GAME } from '../utils/queries';
+// import AddCartBtn from '../components/AddCartBtn'
 
-export default function SingleGame() {
-  const { data, loading } = useQuery(QUERY_SINGLE_GAME, {
-    variables: gameId 
-  })
-  const game = data 
-  return (
-    <>
-      <img class="mx-auto"
-        src={game.image}
-        className='h-1/3 w-1/3 mt-40'
-      />
-      <div class='card'>
-        <Card className="h-auto bg-blue bg-opacity-60 text-grey">
-          <CardBody className="text-center">
-            <Typography variant="h5" className="mb-2">
-              About
-            </Typography>
-            <Typography>
-              {game.title} <br />
-              {game.description}
-            </Typography>
-          </CardBody>
-          <CardFooter divider className="flex items-center justify-between py-3">
-            <Typography variant="small">${game.price}
-            </Typography>
-            <Typography variant="small" color="gray" className="flex gap-1">
-              <i className="fas fa-map-marker-alt fa-sm mt-[3px]" />
-              <AddCartBtn />
-            </Typography>
-          </CardFooter>
-        </Card>
-      </div>
-    </>
-  )
-}
+// export default function SingleGame() {
+//   const { data, loading } = useQuery(QUERY_SINGLE_GAME, {
+//     variables: gameId 
+//   })
+//   const game = data 
+//   return (
+//     <>
+//       <img class="mx-auto"
+//         src={game.image}
+//         className='h-1/3 w-1/3 mt-40'
+//       />
+//       <div class='card'>
+//         <Card className="h-auto bg-blue bg-opacity-60 text-grey">
+//           <CardBody className="text-center">
+//             <Typography variant="h5" className="mb-2">
+//               About
+//             </Typography>
+//             <Typography>
+//               {game.title} <br />
+//               {game.description}
+//             </Typography>
+//           </CardBody>
+//           <CardFooter divider className="flex items-center justify-between py-3">
+//             <Typography variant="small">${game.price}
+//             </Typography>
+//             <Typography variant="small" color="gray" className="flex gap-1">
+//               <i className="fas fa-map-marker-alt fa-sm mt-[3px]" />
+//               <AddCartBtn />
+//             </Typography>
+//           </CardFooter>
+//         </Card>
+//       </div>
+//     </>
+//   )
+// }
